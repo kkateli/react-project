b19245b8c6afa855102eef3ad8acdcea4740e754
kate li <kateli@kates-MBP.hub>
2019-04-18 21:16:42 +1200

    Added button that changes names

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -5,6 +5,19 @@ import "./App.css";
 import Person from './Person/Person';
 
 class App extends Component {
+  //we need to use state with care
+  state = {
+    persons: [{ name: "Kate", age: 28 }, { name: "eli", age: 28 }]
+  };
+  //event handler
+  switchNameHandler = () => {
+    //Dont do this: this.state.persons[0].name = 'Kate changed';
+    //setState will auto overwrite the parts that have been changed
+    //and untouch the identical parts. Changeing state will lead to changing the props
+    this.setState({
+      persons: [{ name: "Kate Li", age: 26 }, { name: "eli", age: 28 }]
+    });
+  };
   render() {
     return (
       /**This code is not HTML, it is JSX. It will be compile to different syntex when runing it. 
@@ -16,14 +29,24 @@ class App extends Component {
       */
       //className is class in css.
       /**Never add anything outside the root element, here is App(Refers to index.js), because a jsx
-      * only allows one root. the best practice is to add css, jsx to the root so 
-      * that we will not ness this around.
-      */
-     //if you are not wrapping anything in a element, we can use /> to self close the element
+       * only allows one root. the best practice is to add css, jsx to the root so
+       * that we will not ness this around.
+       */
+      //if you are not wrapping anything in a element, we can use /> to self close the element
+      //this.switchNameHandler doesnt have (), because we dont
+      //call the function but passing the reference of the function
       <div className="App">
         <h1>I am an app</h1>
-        
-        <Person /> 
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click = {this.switchNameHandler}
+        />
+        <Person name="Eli" age="27"> 
+          I like eating
+        </Person>
+        <button onClick={this.switchNameHandler}>Switch name</button>
       </div>
     );
   }
--- react-project/src/Person/Person.js
+++ react-project/src/Person/Person.js
@@ -1,8 +1,17 @@
 //This is Person component
 import React from 'react';
 
-const person = ()=>{
-    return <p>I am a Person!</p>
-
+const person = (props)=>{
+    //there is the way to declear name and age
+    //wrap what's returned in brakets to have multiple lines
+    //children here is a reserved word to return information we written in the element: 
+    //<Person>here is the info</Person>
+    //we can pass methods as props, this can change the state 
+    //from another file which doesnt have direct access
+    return( 
+        <div><p onClick={props.click}>I am {props.name} I am {props.age}</p>
+        <p>{props.children}</p>
+        </div>
+        )
 }
 export default person;
