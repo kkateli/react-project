1c9f2ab182e61740f5d7d6d514b85292f24811f6
kate li <kateli@kates-MBP.hub>
2019-04-20 23:13:21 +1200

    Adding new

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -5,20 +5,20 @@ import "./App.css";
 import Person from './Person/Person';
 
 class App extends Component {
-  //we need to use state with care
+  // REVIEW (How it is written)we need to use state with care
   state = {
     persons: [{ name: "Kate", age: 28 }, { name: "eli", age: 28 }]
   };
   //event handler
   switchNameHandler = () => {
-    //Dont do this: this.state.persons[0].name = 'Kate changed';
-    //setState will auto overwrite the parts that have been changed
-    //and untouch the identical parts. Changeing state will lead to changing the props
+    //NOTE Dont do this: this.state.persons[0].name = 'Kate changed';
+    /*REVIEW (The way how it is written)setState will auto overwrite the parts that have been changed
+    and untouch the identical parts. Changeing state will lead to changing the props*/
     this.setState({
-      persons: [{ name: "Kate Li", age: 26 }, { name: "eli", age: 28 }]
+      persons: [{ name: "koko", age: 26 }, { name: "eli", age: 28 }]
     });
   };
-//event target is input here, because this function is called be input in Person.js
+//NOTE event target is input here, because this function is called be input in Person.js
   changeNameByInput = (event)=>{
     this.setState({
     persons: [{ name: event.target.value, age: 30 }, { name: "Eli", age: 30 }]
@@ -34,23 +34,24 @@ class App extends Component {
       border:'1px solid blue'
     }
     return (
-      /**This code is not HTML, it is JSX. It will be compile to different syntex when runing it. 
+      /**ANCHOR 1. This code is not HTML, it is JSX. It will be compile to different syntex when runing it. 
        * eg:<div className="App">
             <h1>I am an app</h1>
             </div>
-            can be compiled to return React.creatElement('div',{className:'App},
-            React.createElement(h1,null,'I am an app'));
+      can be compiled to return React.creatElement('div',{className:'App},
+      React.createElement(h1,null,'I am an app'));
+      2. Never add anything outside the root element, here is App(Refers to index.js), because a jsx
+      only allows one root. the best practice is to add css, jsx to the root so
+      that we will not ness this around.
+      3. if you are not wrapping anything in a element, we can use /> to self close the element
+      4. this.switchNameHandler doesnt have (), because we dont call the function 
+      but passing the reference of the function.
+      5. if switchNameHandler has arguments, we can use arrow funtion
+      ()=> this.switchNameHandler(args),this is an anomynous callback function
       */
-      //className is class in css.
-      /**Never add anything outside the root element, here is App(Refers to index.js), because a jsx
-       * only allows one root. the best practice is to add css, jsx to the root so
-       * that we will not ness this around.
-       */
-      //if you are not wrapping anything in a element, we can use /> to self close the element
-      //this.switchNameHandler doesnt have (), because we dont
-      //call the function but passing the reference of the function
-      //if switchNameHandler has arguments, we can use arrow funtion
-      //()=> this.switchNameHandler(args),this is an anomynous callback function
+      //NOTE className is class in css.
+      
+      
       <div className="App">
         <h1>I am an app</h1> 
 
--- react-project/src/Person/Person.js
+++ react-project/src/Person/Person.js
@@ -3,15 +3,16 @@ import React from 'react';
 import "./Person.css";
 
 const person = (props)=>{
-    //there is the way to declear name and age
-    //wrap what's returned in brakets to have multiple lines
-    //children here is a reserved word to return information we written in the element: 
-    //<Person>here is the info</Person>
-    //we can pass methods as props, this can change the state 
-    //from another file which doesnt have direct access
-    //onChange will update if input changes
-    //value={prps.name} enables twoway binding so that the inout knows the current
-    //value
+    /* ANCHOR 1. there is the way to declear name and age
+    2. wrap what's returned in brakets to have multiple lines
+    3. we can pass methods as props, this can change the state 
+    from another file which doesnt have direct access*/
+    
+    /* NOTE children here is a reserved word to return information we written in the element: 
+        <Person>here is the info</Person> */
+   
+    /* NOTE onChange will update if input changes */
+    /* NOTE value={props.name} enables two way binding so that the input knows the current value*/
     return( 
         <div className = 'Person'>
         <p onClick={props.click}>I am {props.name} I am {props.age}</p>
